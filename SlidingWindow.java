c9e237d07d1f27a22a6216084f34b523439f272d
BlunderDev <123478862+vanshika241@users.noreply.github.com>
2025-06-15 22:33:54 +0530

    Update SlidingWindow.java

--- SlidingWindow.java
+++ SlidingWindow.java
@@ -1,3 +1,77 @@
+//Longest substring without repeating character
+public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int i = 0, j = 0;
+        int len = 0;
+        HashMap<Character, Integer> mp = new HashMap<>();
+
+        while (j < n) {
+            mp.put(s.charAt(j), mp.getOrDefault(s.charAt(j), 0) + 1);
+
+            if (mp.size() == j - i + 1) {
+                len = Math.max(len, j - i + 1);
+                j++;
+            }
+            else if (mp.size() < j - i + 1) {
+                while (mp.size() < j - i + 1) {
+                    mp.put(s.charAt(i), mp.get(s.charAt(i)) - 1);
+                    if (mp.get(s.charAt(i)) == 0) {
+                        mp.remove(s.charAt(i));
+                    }
+                    i++;
+                }
+                j++;
+            }
+        }
+
+        return len;
+    }
+//Permutation of string
+    public boolean checkInclusion(String ana, String s) {
+        int n = s.length();
+        int k = ana.length();
+
+        if (k > n) return false;
+
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for (int i = 0; i < ana.length(); i++) {
+            mp.put(ana.charAt(i), mp.getOrDefault(ana.charAt(i), 0) + 1);
+        }
+
+        int i = 0, j = 0, cnt = mp.size();
+
+        while (j < n) {
+            char ch = s.charAt(j);
+            if (mp.containsKey(ch)) {
+                mp.put(ch, mp.get(ch) - 1);
+                if (mp.get(ch) == 0) {
+                    cnt--;
+                }
+            }
+
+            if (j - i + 1 < k) {
+                j++;
+            } else if (j - i + 1 == k) {
+                if (cnt == 0) {
+                    return true;
+                }
+
+                char left = s.charAt(i);
+                if (mp.containsKey(left)) {
+                    mp.put(left, mp.get(left) + 1);
+                    if (mp.get(left) == 1) {
+                        cnt++;
+                    }
+                }
+
+                i++;
+                j++;
+            }
+        }
+
+        return false;
+    }
+
 //Sliding Window Maximum
 public int[] maxSlidingWindow(int[] nums, int k) {
         int n = nums.length;
